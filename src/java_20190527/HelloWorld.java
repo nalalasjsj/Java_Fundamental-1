00c4bc04ef2560721df85b578d7ea7ab38ba2241
yuueuni <yuueuni@gmail.com>
2019-05-27 18:16:10 +0900

    update3

--- src/java_20190527/HelloWorld.java
+++ src/java_20190527/HelloWorld.java
@@ -2,10 +2,8 @@ package java_20190527;
 
 public class HelloWorld {
 	public static void main(String[] args){
-		System.out.println("Hello World");
-		System.out.println("Hello World123");
-		System.out.println("Hello World12dd3");
-		System.out.println("Hello World12dd1233");
-		System.out.println("Hello World12dd124443");
+		System.out.println(1+2+3+"java");
+		System.out.println("java"+1+2+3);
+		System.out.println("java"+(1+2+3));
 	}
 }
