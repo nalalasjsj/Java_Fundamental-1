35cc9b638927561a1ff3beaba17564eb6f7a5141
Administrator <Administrator@AUTOBVT-GPK20B1>
2019-05-28 21:45:34 +0900

    Testjava update

--- src/java_20190527/HelloWorld.java
+++ src/java_20190527/HelloWorld.java
@@ -3,7 +3,7 @@ package java_20190527;
 public class HelloWorld {
 	public static void main(String[] args){
 		System.out.println(1+2+3+"java");
-		System.out.println("java"+1+2+3);
+		System.out.println("java"+1+2+3+4+5);
 		System.out.println("java"+(1+2+3));
 	}
 }
